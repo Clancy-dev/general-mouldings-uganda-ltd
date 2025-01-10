aadfa3128a3d731b29c33d8c00e2f5250979aa47
Clancy-dev <clancyro1789@gmail.com>
2025-01-10 17:20:54 +0300

    the

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -5,8 +5,9 @@ import React from 'react'
 export default function page() {
   return (
     <div className='mt-[72px]'>
-    <Services />
-    <Products />
+      <p>Being Worked On...</p>
+    {/* <Services />
+    <Products /> */}
     </div>
   )
 }
